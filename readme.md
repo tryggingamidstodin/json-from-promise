cab862fc579314b3fa4307f0996c382855b08bc7
jakob <jakob.runarsson@gmail.com>
2016-08-11 19:37:55 +0000

    renmae

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
-response-with-promise
-=====================
+json-from-promise
+=====
 
 Example
 ```javascript
 const app = require('express')();
-const withPromise = require('response-with-promise');
-app.get('/ping/:msg', withPromise(req => Promise.resolve('pong' + req.params.id)));
+const jsonFromPromise = require('json-from-promise');
+app.get('/ping/:msg', jsonFromPromise(req => Promise.resolve({pong: req.params.id})));
 ```
--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 'use strict';
-const withPromise = require('../');
+const jsonFromPromise = require('../');
 const express = require('express');
 const request = require('supertest');
 
 describe('express', function() {
     it('should respond', function(done) {
         const app = express();
-        app.get('/ping/:msg', withPromise(req => Promise.resolve({pong: req.params.msg})));
+        app.get('/ping/:msg', jsonFromPromise(req => Promise.resolve({pong: req.params.msg})));
         request(app)
             .get('/ping/foo')
             .expect(200, {
@@ -15,7 +15,7 @@ describe('express', function() {
     });
     it('should return 500 on error', function(done) {
         const app = express();
-        app.get('/ping/:msg', withPromise(() => Promise.reject(new Error('testerror'))));
+        app.get('/ping/:msg', jsonFromPromise(() => Promise.reject(new Error('testerror'))));
         request(app)
             .get('/ping/foo')
             .expect(500, done);
